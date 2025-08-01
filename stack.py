6580a95ead7aad0c7c8fdb7f34ce31363a7b3d1b
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-08-01 19:43:55 -0400

    stack.py in place

--- stack.py
+++ stack.py
@@ -3,5 +3,28 @@ def is_valid_parentheses(s: str) -> bool:
     Return True if the string contains valid, balanced parentheses.
     Only (), {}, and [] are considered valid.
     """
-    # TODO: Implement stack logic to validate parentheses
-    pass
+    stack = []
+    matching = {')', '(', '}', '{', ']', '['}
+
+    for character in s:
+        if character in matching.values():
+            stack.append(character)
+        elif character in matching:
+            if not stack or stack[-1] != matching[character]:
+                return False
+            stack.pop()
+
+    return not stack
+
+# Create an empty list to simulate stack
+# Create a dict to map closing -> opening brackets
+# Loop through the input string
+    # If its an opening bracket, push to stack
+    # If its a closing bracket:
+        # If the stack is empty, return false
+        # If the top of the stack doesnt match the cooresponding opening, return false
+        # Otherwise, pop the opening
+    # After the loop:
+        # If yes, return True
+        # If not, return False
+
